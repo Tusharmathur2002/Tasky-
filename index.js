3010d5641c78d7d48f37ecd40aa11eb6c0c29d2b
Tushar <mathurtushar2002@gmail.com>
2023-07-15 14:42:24 +0530

    fix refresh issue

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const taskContainer = document.querySelector(".task_container");
+//Global store
+const globalStore = [];
+// card 1, card 2--> array
 
 const newCard = ({ id, imageUrl, taskTitle, taskType, taskDescription }) =>
     `<div class="col-md-6 col-lg-4" id=${id}>
@@ -22,6 +25,20 @@ const newCard = ({ id, imageUrl, taskTitle, taskType, taskDescription }) =>
 </section>
 </div>`;
 
+
+const loadInitialTaskCards = () => {
+  //access local storage
+const getInitialData = localStorage.getItem("tasky");
+if(!getInitialData) return;
+  //convert string object to object
+const { cards } = JSON.parse(getInitialData);
+  //map around the array to generate HTML card and injict it to DOM
+cards.map((cardObject) => {
+  const createNewCard = newCard(cardObject);
+  taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+  globalStore.push(cardObject);
+});
+}
 const saveChanges = () => {
     const taskData = {
         id: `${Date.now()}`,
@@ -32,8 +49,16 @@ const saveChanges = () => {
     };
     const createNewCard = newCard(taskData);
     taskContainer.insertAdjacentHTML("beforeend", createNewCard);
-}; 
-// The modal was not closing upon adding a new card
-//Delete modal feature
+    globalStore.push(taskData);
+    console.log(globalStore);
 
-//Features
+    //API(Application Programming Interface)
+    //localstorage-->interface-->programming
+
+
+    localStorage.setItem("tasky",JSON.stringify({cards: globalStore}));
+    //tasky--> is a keyto provide a unique to identify in local storage
+    //setItem--> a function used to set items in local storage
+
+
+}; 
