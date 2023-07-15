50b55eedc6916415fe55b45340a48468344b5902
Tushar <mathurtushar2002@gmail.com>
2023-07-15 17:32:23 +0530

    add deletion feature

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const taskContainer = document.querySelector(".task_container");
 //Global store
-const globalStore = [];
+let globalStore = [];
 // card 1, card 2--> array
 
 const newCard = ({ id, imageUrl, taskTitle, taskType, taskDescription }) =>
-    `<div class="col-md-6 col-lg-4" id=${id}>
+  `<div class="col-md-6 col-lg-4" id=${id}>
 <section><div class="card">
     <div class="card-header d-flex justify-content-end gap-2">
       <button type="button" class="btn btn-outline-success"><i class="fa-sharp fa-solid fa-pencil"></i></button>
-<button type="button" class="btn btn-outline-danger"><i class="fa-solid fa-trash"></i></button>
+<button type="button" id=${id} class="btn btn-outline-danger" onclick ="deleteCard.apply(this, arguments)"><i class="fa-solid fa-trash" onclick ="deleteCard.apply(this, arguments)"></i></button>
     </div>
     <img src=${imageUrl}
     class="card-img-top" 
@@ -28,37 +28,75 @@ const newCard = ({ id, imageUrl, taskTitle, taskType, taskDescription }) =>
 
 const loadInitialTaskCards = () => {
   //access local storage
-const getInitialData = localStorage.getItem("tasky");
-if(!getInitialData) return;
+  const getInitialData = localStorage.getItem("tasky");
+  if (!getInitialData) return;
+
   //convert string object to object
-const { cards } = JSON.parse(getInitialData);
+  const { cards } = JSON.parse(getInitialData);
+
+
   //map around the array to generate HTML card and injict it to DOM
-cards.map((cardObject) => {
-  const createNewCard = newCard(cardObject);
-  taskContainer.insertAdjacentHTML("beforeend", createNewCard);
-  globalStore.push(cardObject);
-});
-}
-const saveChanges = () => {
-    const taskData = {
-        id: `${Date.now()}`,
-        imageUrl: document.getElementById("ImageUrl").value,
-        taskTitle: document.getElementById("Tasktitle").value,
-        taskType: document.getElementById("tasktype").value,
-        taskDescription: document.getElementById("taskdescription").value,
-    };
-    const createNewCard = newCard(taskData);
+  cards.map((cardObject) => {
+    const createNewCard = newCard(cardObject);
     taskContainer.insertAdjacentHTML("beforeend", createNewCard);
-    globalStore.push(taskData);
-    console.log(globalStore);
+    globalStore.push(cardObject);
+  });
+};
 
-    //API(Application Programming Interface)
-    //localstorage-->interface-->programming
+const updateLocalStorage = () =>
+  localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
+
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`,
+    imageUrl: document.getElementById("ImageUrl").value,
+    taskTitle: document.getElementById("Tasktitle").value,
+    taskType: document.getElementById("tasktype").value,
+    taskDescription: document.getElementById("taskdescription").value,
+  };
+  //HTML code
+  const createNewCard = newCard(taskData);
+
+  taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+  globalStore.push(taskData);
 
 
-    localStorage.setItem("tasky",JSON.stringify({cards: globalStore}));
-    //tasky--> is a keyto provide a unique to identify in local storage
-    //setItem--> a function used to set items in local storage
+  //add to local storage
+  updateLocalStorage();
+
+  //API(Application Programming Interface)
+  //localstorage-->interface-->programming
+  //tasky--> is a keyto provide a unique to identify in local storage
+  //setItem--> a function used to set items in local storage
 
 
-}; 
+};
+
+const deleteCard = (event) => {
+
+  //id
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName; //BUTTON
+  console.log(targetID);
+
+  //search the global store , remove the object which has the id
+  globalStore = globalStore.filter(
+    (cardObject) => !cardObject.id !== targetID);
+
+  updateLocalStorage();
+
+  //access the DOM to delete
+
+  if (tagname === "BUTTON") {
+    //task_container
+    return event.target.parentNode.parentNode.parentNode.parentNode.removeChild(
+      event.target.parentNode.parentNode.parentNode);  //col-lg-4
+  }
+
+
+  //task_container
+  return event.target.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+    event.target.parentNode.parentNode.parentNode.parentNode);
+
+};
